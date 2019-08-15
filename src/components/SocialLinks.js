7015e83a600ee2c0cb4f170b2a0db8703d4b763b
Nelson Estevão <nelsonmestevao@gmail.com>
2019-08-15 23:29:19 +0100

    Move component to function

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -7,25 +7,21 @@ import {
   faGithub,
 } from '@fortawesome/free-brands-svg-icons';
 
-class SocialLinks extends React.Component {
-  render() {
+export default function SocialLinks(props) {
     return (
       <div className="social-container">
-        <a href={`https://www.facebook.com/${this.props.facebook}`} className="facebook social-link">
+        <a href={`https://www.facebook.com/${props.facebook}`} className="facebook social-link">
           <FontAwesomeIcon icon={faFacebook} size="2x" />
         </a>
-        <a href={`https://www.twitter.com/${this.props.twitter}`} className="twitter social-link">
+        <a href={`https://www.twitter.com/${props.twitter}`} className="twitter social-link">
           <FontAwesomeIcon icon={faTwitter} size="2x" />
         </a>
-        <a href={`https://www.instagram.com/${this.props.instagram}`} className="instagram social-link">
+        <a href={`https://www.instagram.com/${props.instagram}`} className="instagram social-link">
           <FontAwesomeIcon icon={faInstagram} size="2x" />
         </a>
-        <a href={`https://www.github.com/${this.props.github}`} className="github social-link">
+        <a href={`https://www.github.com/${props.github}`} className="github social-link">
           <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
       </div>
     );
-  }
 }
-
-export default SocialLinks;
