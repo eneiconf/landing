a5b0f346a9772fcea07f37a74065ea272f09bba5
Nelson Estevão <nelsonmestevao@gmail.com>
2019-08-16 12:26:32 +0100

    Move to arrow function

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -7,23 +7,21 @@ import {
   faGithub,
 } from '@fortawesome/free-brands-svg-icons';
 
-function SocialLinks({ facebook, twitter, instagram, github }) {
-  return (
-    <div className="social-container">
-      <a href={`https://www.facebook.com/${facebook}`} className="facebook social-link">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-      </a>
-      <a href={`https://www.twitter.com/${twitter}`} className="twitter social-link">
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
-      </a>
-      <a href={`https://www.instagram.com/${instagram}`} className="instagram social-link">
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-      </a>
-      <a href={`https://www.github.com/${github}`} className="github social-link">
-        <FontAwesomeIcon icon={faGithub} size="2x" />
-      </a>
-    </div>
-  );
-}
+const SocialLinks = ({ facebook, twitter, instagram, github }) => (
+  <div className="social-container">
+    <a href={`https://www.facebook.com/${facebook}`} className="facebook social-link">
+      <FontAwesomeIcon icon={faFacebook} size="2x" />
+    </a>
+    <a href={`https://www.twitter.com/${twitter}`} className="twitter social-link">
+      <FontAwesomeIcon icon={faTwitter} size="2x" />
+    </a>
+    <a href={`https://www.instagram.com/${instagram}`} className="instagram social-link">
+      <FontAwesomeIcon icon={faInstagram} size="2x" />
+    </a>
+    <a href={`https://www.github.com/${github}`} className="github social-link">
+      <FontAwesomeIcon icon={faGithub} size="2x" />
+    </a>
+  </div>
+);
 
 export default SocialLinks;
