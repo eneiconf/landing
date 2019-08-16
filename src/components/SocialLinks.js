8ef8dc5f01077d472b615b6fe88cc4c4e8e1079c
Nelson Estevão <nelsonmestevao@gmail.com>
2019-08-16 12:03:03 +0100

    Update with review suggestions

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -7,21 +7,23 @@ import {
   faGithub,
 } from '@fortawesome/free-brands-svg-icons';
 
-export default function SocialLinks(props) {
-    return (
-      <div className="social-container">
-        <a href={`https://www.facebook.com/${props.facebook}`} className="facebook social-link">
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
-        </a>
-        <a href={`https://www.twitter.com/${props.twitter}`} className="twitter social-link">
-          <FontAwesomeIcon icon={faTwitter} size="2x" />
-        </a>
-        <a href={`https://www.instagram.com/${props.instagram}`} className="instagram social-link">
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
-        </a>
-        <a href={`https://www.github.com/${props.github}`} className="github social-link">
-          <FontAwesomeIcon icon={faGithub} size="2x" />
-        </a>
-      </div>
-    );
+function SocialLinks({ facebook, twitter, instagram, github }) {
+  return (
+    <div className="social-container">
+      <a href={`https://www.facebook.com/${facebook}`} className="facebook social-link">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href={`https://www.twitter.com/${twitter}`} className="twitter social-link">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a href={`https://www.instagram.com/${instagram}`} className="instagram social-link">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+      <a href={`https://www.github.com/${github}`} className="github social-link">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+    </div>
+  );
 }
+
+export default SocialLinks;
