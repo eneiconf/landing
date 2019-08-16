961119dc2e5a919031cb52ac76e0757486cf8bdd
Nelson Estevão <nelsonmestevao@gmail.com>
2019-08-16 10:37:41 +0100

    Refactor app

--- src/components/SocialLinks.test.js
+++ src/components/SocialLinks.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SocialLinks from './SocialLinks';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<SocialLinks />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.js
+++ src/index.js
@@ -3,12 +3,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-// COMPONENTS
-import App from './components/App';
-
 // STYLES
 import 'normalize.css';
 import './styles/app.scss';
+
+// RESOURCES
+import Logo from './images/logo.png';
+
+// COMPONENTS
+import SocialLinks from './components/SocialLinks';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <div className="logo">
+          <img alt="logo do evento" src={Logo} />
+        </div>
+        <div className="text">
+          <h1 className="title">Encontro Nacional de Estudantes de Informática</h1>
+          <h2 className="date">23 a 26 de Fevereiro 2020</h2>
+          <h3 className="location">Braga, Portugal</h3>
+        </div>
+        <SocialLinks facebook="ENEIConf" twitter="eneiconf" instagram="eneiconf" github="eneiconf" />
+        <div className="darken">
+          <div id="backgroundchange">
+            <div className="backgroundimg" id="back1" />
+            <div className="backgroundimg" id="back2" />
+            <div className="backgroundimg" id="back3" />
+            <div className="backgroundimg" id="back4" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
